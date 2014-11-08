3156786caec405d345e8c5fa80428e750a977903
Gnaarf <gnaarf@gmx.de>
2014-11-08 18:40:25 +0100

    fixed Tuans bug

---
+++ Assets/shaker.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shaker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.transform.position += new Vector3(Mathf.Sin(Time.time * 20) / 5, 0, 0);
+	}
+}

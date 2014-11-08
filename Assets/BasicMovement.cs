90875e6676726f6fc6886df92401440e85efdd81
Tuan Pham Minh <gearhunter@gmx.de>
2014-11-08 18:38:19 +0100

    basic movement script block prefab

---
+++ Assets/BasicMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        const float SPEED = 0.5f;
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * SPEED;
+        transform.position += movement.x * transform.right + movement.y * transform.forward;
+	}
+}

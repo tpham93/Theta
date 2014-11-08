c86f5d8880e82ccac7335a4885e788915f65f9d4
Tuan Pham Minh <gearhunter@gmx.de>
2014-11-08 20:46:50 +0100

    functioning tetris block generation

---
+++ Assets/Block.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+public class Block
+{
+    public Vector3 offset;
+    public GameObject blockObject;
+
+    public Block(GameObject blockObject, Vector3 offset)
+    {
+        this.offset = offset;
+        this.blockObject = blockObject;
+    }
+}
---
+++ Assets/TetrisBlock.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class TetrisBlock
+{
+    List<Block> blocks;
+    public TetrisBlock()
+        :this(new List<Block>())
+    {
+    }
+    public TetrisBlock(List<Block> blocks)
+    {
+        this.blocks = blocks;
+    }
+
+    public void BasicMovement(Vector3 movement)
+    {
+        blocks[0].blockObject.transform.position += movement;
+    }
+}

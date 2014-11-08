10889cb673f7e162133c41d97ccdc9472feba242
Gnaarf <gnaarf@gmx.de>
2014-11-08 21:06:02 +0100

    finished Testlevel v0.1

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Vector3 position { get { return this.transform.position; } }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}

629095fbfb167f62c63ca69e5be7b312d38a0796
Gnaarf <gnaarf@gmx.de>
2014-11-08 19:44:32 +0100

    working on test scene for player

--- Assets/shaker.cs
+++ Assets/shaker.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class shaker : MonoBehaviour {
 
+    private Vector3 start;
+    private float stepSize
+    {
+        get { return speed / Vector3.Distance(start, target); }
+    }
+    private bool ascending = true;
+
+    public Vector3 target = new Vector3(0, 0, 0);
+    public float speed = 1;
+    public float progress = 0;
+
 	// Use this for initialization
 	void Start () {
-	
+        start = this.transform.position;
+        if (progress != 0)
+            this.transform.position = Vector3.Lerp(start, target, progress);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        this.gameObject.transform.position += new Vector3(Mathf.Sin(Time.time * 20) / 5, 0, 0);
+        if (progress > 1)
+            ascending = false;
+        else if (progress < 0)
+            ascending = true;
+
+        if (ascending)
+            progress += stepSize * Time.deltaTime;
+        else
+            progress -= stepSize * Time.deltaTime;
+
+        this.transform.position = Vector3.Lerp(start, target, progress);
+
 	}
 }

dbc8808c62dead3b9579e2932f95d686cad3a2ac
Gnaarf <gnaarf@gmx.de>
2014-11-09 01:01:28 +0100

    PlayerController works goodish

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -14,30 +14,34 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float stepSize = speed * Time.deltaTime;
-        
+
         // move
         this.gameObject.transform.position += Input.GetAxis("Horizontal") * Vector3.right * stepSize;
         this.gameObject.transform.position += Input.GetAxis("Vertical") * Vector3.forward * stepSize;
-        
+
         // don't fall over
         this.gameObject.rigidbody.MoveRotation(Quaternion.identity);
-        
+
         // jump
-        if(!isJumping && Input.GetAxis("Jump") == 1)
+        if (!isJumping && Input.GetAxis("Jump") == 1)
         {
             this.gameObject.rigidbody.AddExplosionForce(800.0f, position + Vector3.down, 10.0f);
             isJumping = true;
         }
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
     void OnCollisionEnter(Collision collision)
     {
         if(isJumping && collision.collider.name == "Block" && collision.collider.transform.position.y < position.y)
             isJumping = false;
     }
-
 }

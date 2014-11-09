03fc0cb2ee732475db9f21bb1326cd1b34698050
Tuan Pham Minh <gearhunter@gmx.de>
2014-11-09 13:37:36 +0100

    functioning tetris manager

--- Assets/TetrisBlock.cs
+++ Assets/TetrisBlock.cs
@@ -21,9 +21,51 @@ public class TetrisBlock
         this.blocks = blocks;
     }
 
+    public Rect getBounds()
+    {
+        Vector3 min = new Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity);
+        Vector3 max = new Vector3(float.NegativeInfinity, float.NegativeInfinity, float.NegativeInfinity);
+
+        for (int i = 0; i < 4; ++i )
+        {
+            if (blocks[i].Offset.x < min.x)
+            {
+                min.x = blocks[i].Offset.x;
+            }
+            if (blocks[i].Offset.y < min.y)
+            {
+                min.y = blocks[i].Offset.y;
+            }
+            if (blocks[i].Offset.z < min.z)
+            {
+                min.z = blocks[i].Offset.z;
+            }
+
+
+            if (blocks[i].Offset.x > max.x)
+            {
+                max.x = blocks[i].Offset.x;
+            }
+            if (blocks[i].Offset.y > max.y)
+            {
+                max.y = blocks[i].Offset.y;
+            }
+            if (blocks[i].Offset.z > max.z)
+            {
+                max.z = blocks[i].Offset.z;
+            }
+        }
+        float x = min.x;
+        float y = min.z;
+        float width = max.x - min.x;
+        float height = max.z - min.z;
+
+        return new Rect(x,y, width, height);
+    }
+
     public void move(Vector3 movement)
     {
         blocks[0].BlockObject.transform.position += movement;
     }
 
-}
+} 
--- Assets/TetrisManagerScript.cs
+++ Assets/TetrisManagerScript.cs
@@ -30,27 +30,31 @@ public class TetrisManagerScript : MonoBehaviour
                 }
             }
         }
-        currentMovingBlock = spawnTetrisBlock(new Vector3(SampleBlock.transform.lossyScale.x, 0, 20 * SampleBlock.transform.lossyScale.z));
+        currentMovingBlock = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //lastSpawnedBlockTime += Time.deltaTime;
-        //if (lastSpawnedBlockTime > 2)
-        //{
-        //    currentMovingBlock = spawnTetrisBlock(new Vector3(0, 0,20));
-        //    lastSpawnedBlockTime = 0;
-        //}
         bool hasToStop = false;
         Vector3 moveOffset = new Vector3(0.0f, 0.0f, -0.1f * SampleBlock.transform.lossyScale.z);
+
+        if (currentMovingBlock == null)
+        {
+            currentMovingBlock = spawnTetrisBlock(new Vector3(0, 0, 0));
+            Rect bounds = currentMovingBlock.getBounds();
+            Debug.Log(bounds);
+            currentMovingBlock.move(new Vector3(-bounds.x, 0, bounds.y + MAP_LENGTH * SampleBlock.transform.lossyScale.z));
+        }
+
+
         for (int i = 0; i < currentMovingBlock.Blocks.Count; ++i)
         {
             Block currentBlock = currentMovingBlock.Blocks[i];
             Vector3 scale = currentBlock.BlockObject.transform.lossyScale;
             Vector3 currentBlockLeftBottom = currentBlock.BlockObject.transform.position;
             Vector3 mapCoordinate = new Vector3(currentBlockLeftBottom.x / scale.x, currentBlockLeftBottom.y / scale.y, (currentBlockLeftBottom.z + moveOffset.z - scale.z/2) / scale.z);
-            hasToStop |= (mapCoordinate.z) < 0 || ((int)(mapCoordinate.z) < MAP_LENGTH && map[(int)(mapCoordinate.x), (int)(mapCoordinate.y), (int)  (mapCoordinate.z)] != null);
+            hasToStop |= (mapCoordinate.z) < 0 || ((int)(mapCoordinate.z) < MAP_LENGTH && map[(int)(mapCoordinate.x), (int)(mapCoordinate.y), (int)(mapCoordinate.z)] != null);
         }
 
         if (hasToStop)
@@ -62,10 +66,10 @@ public class TetrisManagerScript : MonoBehaviour
 
                 Vector3 scale = currentBlock.BlockObject.transform.lossyScale;
                 Vector3 currentBlockLeftBottom = currentBlock.BlockObject.transform.position;
-                Vector3 mapCoordinate = new Vector3(currentBlockLeftBottom.x / scale.x, currentBlockLeftBottom.y / scale.y, (currentBlockLeftBottom.z - scale.z/2) / scale.z);
+                Vector3 mapCoordinate = new Vector3(currentBlockLeftBottom.x / scale.x, currentBlockLeftBottom.y / scale.y, (currentBlockLeftBottom.z) / scale.z);
                 map[(int)(mapCoordinate.x), (int)(mapCoordinate.y), (int)(mapCoordinate.z)] = currentBlock;
             }
-            currentMovingBlock = spawnTetrisBlock(new Vector3(SampleBlock.transform.lossyScale.x, 0, 20 * SampleBlock.transform.lossyScale.z));
+            currentMovingBlock = null;
         }
         else
         {
@@ -78,7 +82,7 @@ public class TetrisManagerScript : MonoBehaviour
 
             for (int x = 0; x < MAP_WIDTH; ++x)
             {
-                line += map[x,0,MAP_LENGTH - y - 1] == null? '_' : 'x';
+                line += map[x, 0, MAP_LENGTH - y - 1] == null ? '_' : 'x';
             }
             Debug.Log(line);
         }
@@ -94,23 +98,26 @@ public class TetrisManagerScript : MonoBehaviour
     private TetrisBlock spawnTetrisBlock(Vector3 startPosition)
     {
         List<Block> blocks = new List<Block>();
-        Block[,] blocksMap = new Block[4, 2];
+        Block[,] blocksMap = new Block[4, 4];
         List<Vector2> possiblePositions = new List<Vector2>();
         blocks.Add(new Block((GameObject)GameObject.Instantiate(SampleBlock, startPosition, Quaternion.identity), Vector3.zero));
-        blocksMap[1, 0] = blocks[0];
-        possiblePositions.Add(new Vector2(0, 0));
-        possiblePositions.Add(new Vector2(1, 1));
-        possiblePositions.Add(new Vector2(2, 0));
+        blocksMap[1, 1] = blocks[0];
+        possiblePositions.Add(new Vector2(1, 0));
+        possiblePositions.Add(new Vector2(0, 1));
+        possiblePositions.Add(new Vector2(2, 1));
+        possiblePositions.Add(new Vector2(1, 2));
         System.Random random = new System.Random();
+
         for (int i = 1; i < 4; ++i)
         {
             int positionIndex = random.Next(possiblePositions.Count);
             Vector2 position = possiblePositions[positionIndex];
             possiblePositions.RemoveAt(positionIndex);
 
-            Vector2 offset = position - new Vector2(1, 0);
+            Vector2 offset = position - new Vector2(1, 1);
+            Vector3 worldOffset = offset2DToVector3D(offset);
 
-            blocks.Add(new Block((GameObject)GameObject.Instantiate(SampleBlock, startPosition + offset2DToVector3D(offset), Quaternion.identity), offset2DToVector3D(offset)));
+            blocks.Add(new Block((GameObject)GameObject.Instantiate(SampleBlock, startPosition + worldOffset, Quaternion.identity), worldOffset));
             blocks[blocks.Count - 1].BlockObject.transform.parent = blocks[0].BlockObject.transform;
             blocksMap[(int)position.x, (int)position.y] = blocks[i];
 
@@ -129,17 +136,6 @@ public class TetrisManagerScript : MonoBehaviour
                 }
             }
         }
-
-        for (int i = 0; i < 4; ++i)
-        {
-            int numStackingBlocks = (int)(random.NextDouble() * random.NextDouble() * random.NextDouble() * MAP_HEIGHT);
-            for (int s = 1; s <= numStackingBlocks; ++s)
-            {
-                blocks.Add(new Block((GameObject)GameObject.Instantiate(SampleBlock, blocks[i].BlockObject.transform.position + new Vector3(0, s * SampleBlock.transform.lossyScale.y, 0), Quaternion.identity), offset2DToVector3D(new Vector3(0, s))));
-                blocks[blocks.Count - 1].BlockObject.transform.parent = blocks[i].BlockObject.transform;
-            }
-        }
-
         TetrisBlock newBlock = new TetrisBlock(blocks);
         return newBlock;
     }

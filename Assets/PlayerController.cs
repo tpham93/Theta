9ee591f313a2fe9652862f8b70e55e48ca70f6fe
Hawkcutter <jarek.business@googlemail.com>
2014-11-09 12:52:27 +0100

    Multiplayer! - support up to 4 players - 2 players can use keyboard, up to 4 players can use gamepad

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public int ID1 = 1;
+    private int ID2 = 1;
     public float speed = 1;
     private Vector3 position { get { return this.transform.position; } }
     private bool isJumping = false;
@@ -12,26 +13,48 @@ public class PlayerController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        switch (ID1)
+        {
+            case 1:
+                ID2 = MainMenu.KeysP1;
+                break;
+            case 2: 
+                ID2 = MainMenu.KeysP2;
+                break;
+            case 3:
+                ID2 = MainMenu.KeysP3;
+                break;
+            case 4:
+                ID2 = MainMenu.KeysP4;
+                break;
+        }
     }
 
     void FixedUpdate()
     {
         float stepSize = speed * Time.deltaTime;
 
-        // move
-        this.gameObject.transform.position += Input.GetAxis("Horizontal") * Vector3.right * stepSize;
-        this.gameObject.transform.position += Input.GetAxis("Vertical") * Vector3.forward * stepSize;
 
-        // don't fall over
-        this.gameObject.rigidbody.MoveRotation(Quaternion.identity);
-
-        // jump
-        if (!isJumping && Input.GetAxis("Jump") == 1)
+        if (0 < ID2 && ID2 < 7)
         {
-            this.gameObject.rigidbody.AddExplosionForce(800.0f, position + Vector3.down, 10.0f);
-            isJumping = true;
-            this.transform.parent = null;
-            currentBlock = null;
+            // move
+            if (Mathf.Abs(Input.GetAxis("Horizontal" + ID2))> 0.9f)
+                this.gameObject.transform.position += Input.GetAxis("Horizontal" + ID2) * Vector3.right * stepSize;
+
+            if (Mathf.Abs(Input.GetAxis("Vertical" + ID2)) > 0.9f)
+                this.gameObject.transform.position += Input.GetAxis("Vertical" + ID2) * Vector3.forward * stepSize;
+
+            // don't fall over
+            this.gameObject.rigidbody.MoveRotation(Quaternion.identity);
+
+            // jump
+            if (!isJumping && Input.GetAxis("Jump" + ID2) == 1)
+            {
+                this.gameObject.rigidbody.AddExplosionForce(800.0f, position + Vector3.down, 10.0f);
+                isJumping = true;
+                this.transform.parent = null;
+                currentBlock = null;
+            }
         }
     }
 
